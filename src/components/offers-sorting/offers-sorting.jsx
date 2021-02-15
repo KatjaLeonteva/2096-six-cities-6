2513410b1b09f7be6250085e41bab657aa65eb65
Ekaterina Leonteva <ekaterina_leonteva@epam.com>
2021-02-15 23:35:20 +0100

    Реализует выбор варианта сортировки.

--- src/components/offers-sorting/offers-sorting.jsx
+++ src/components/offers-sorting/offers-sorting.jsx
@@ -1,21 +1,53 @@
-import React from 'react';
+import React, {useState, useEffect, createRef} from 'react';
+
+import {SortingTypes} from '../../const';
 
 const OffersSorting = () => {
+  const [opened, setOpened] = useState(false);
+  const [active, setActive] = useState(SortingTypes[0]);
+  const formNode = createRef();
+
+  const handleDropdownClick = () => {
+    setOpened((prevState) => !prevState);
+  };
+
+  const handleItemClick = (evt) => {
+    setActive(evt.target.innerText);
+    setOpened(false);
+  };
+
+  const handleClickOutside = (evt) => {
+    if (formNode.current.contains(evt.target)) {
+      return;
+    }
+    setOpened(false);
+  };
+
+  useEffect(() => {
+    if (opened) {
+      document.addEventListener(`mousedown`, handleClickOutside);
+    } else {
+      document.removeEventListener(`mousedown`, handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener(`mousedown`, handleClickOutside);
+    };
+  }, [opened]);
 
   return (
-    <form className="places__sorting" action="#" method="get">
+    <form className="places__sorting" action="#" method="get" ref={formNode} style={{display: `inline-block`}}>
       <span className="places__sorting-caption">Sort by&nbsp;</span>
-      <span className="places__sorting-type" tabIndex="0">
-        Popular
+      <span className="places__sorting-type" tabIndex="0" onClick={handleDropdownClick}>
+        {active}
         <svg className="places__sorting-arrow" width="7" height="4">
           <use xlinkHref="#icon-arrow-select"></use>
         </svg>
       </span>
-      <ul className="places__options places__options--custom">
-        <li className="places__option places__option--active" tabIndex="0">Popular</li>
-        <li className="places__option" tabIndex="0">Price: low to high</li>
-        <li className="places__option" tabIndex="0">Price: high to low</li>
-        <li className="places__option" tabIndex="0">Top rated first</li>
+      <ul className={`places__options places__options--custom ${opened ? `places__options--opened` : ``}`}>
+        {SortingTypes.map((item, i) => (
+          <li className={`places__option ${active === item ? `places__option--active` : ``}`} tabIndex="0" key={`sorting${i}`} onClick={handleItemClick}>{item}</li>
+        ))}
       </ul>
     </form>
   );
