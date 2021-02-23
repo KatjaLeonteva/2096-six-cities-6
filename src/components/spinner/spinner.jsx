14e75dde224f787863886fcaa409c51c134e9c0e
Ekaterina Leonteva <ekaterina_leonteva@epam.com>
2021-02-23 18:51:20 +0100

    Исправляет ошибку в Spinner.

--- src/components/spinner/spinner.jsx
+++ src/components/spinner/spinner.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Spinner = (text = `Loading...`) => {
+const Spinner = ({text = `Loading...`}) => {
   return (
     <div>{text}</div>
   );
+};
+
+Spinner.propTypes = {
+  text: PropTypes.string
 };
 
 export default Spinner;
